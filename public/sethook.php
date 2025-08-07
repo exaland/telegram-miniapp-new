4cddd37008d8ebfbb990f71dadcf1b12467b0e9a
Alexandre MAGNIER <exaland@gmail.com>
2025-08-07 22:34:48 +0000

    update sethook.php with direct value

--- public/sethook.php
+++ public/sethook.php
@@ -1,10 +1,10 @@
 <?php declare(strict_types=1);
 
 $result = \TelegramBot\Request::setWebhook([
-    'bot_token' => $_ENV['TELEGRAM_BOT_TOKEN'],
-    'url' => $_ENV['REMOTE_PATH'] . '/telegram',
+    'bot_token' => "7731229792:AAEFfAlMpgWWk_MeQ53nTdnmTfey0ElQ_7M",
+    'url' => "https://creertonbot-ab19d53aadb0.herokuapp.com/telegram",
     'drop_pending_updates' => true,
 ]);
 
-echo $_ENV['REMOTE_PATH'] . '/telegram' . PHP_EOL;
+echo "https://creertonbot-ab19d53aadb0.herokuapp.com/telegram" . PHP_EOL;
 echo $result->isOk() ? 'Webhook set successfully!' : 'Webhook set failed!';
